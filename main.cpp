a2b0ad349e20b3ad3cb642c5c3931d6c1ce6d3cf
Melpomenna <melpomeNn@yandex.ru>
2025-04-10 22:50:00 +0700

    Init

---
+++ main.cpp
@@ -0,0 +1,8 @@
+﻿#define WIN32_LEAN_AND_MEAN  
+#include <windows.h>  
+
+int APIENTRY WinMain([[maybe_unused]] _In_ HINSTANCE hInstance,
+	[[maybe_unused]]  _In_opt_ HINSTANCE hPrevInstance, [[maybe_unused]]  _In_ LPSTR lpCmdLine, [[maybe_unused]]  _In_ int nCmdShow)
+{  
+   return 0;  
+}
